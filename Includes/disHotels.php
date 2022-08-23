95b5aa2576a87932c17663532cd641486e0520b7
Jesse <jesse@JoelOslo.local>
2022-08-23 13:57:55 +0200

    html intake

--- Includes/disHotels.php
+++ Includes/disHotels.php
@@ -35,13 +35,35 @@
         ),
     ];
 
-    foreach($_SESSION['hotelList'] as $listHotel)
-    {
-        echo $listHotel->gethotelName();
-        echo $listHotel->getcostperNight();
-        echo $listHotel->getimage();
-        echo $listHotel->getTotalCost();
-    };
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php
+
+            foreach($_SESSION['hotelList'] as $listHotel)
+            {
+                echo"
+                    <div>
+                        <li>Hotel Name :".$listHotel->gethotelName()."</li>
+                        <li>Cost per night:".$listHotel->getcostperNight()."</li>
+                        <li>".$listHotel->getimage()."</li>
+                        <li>Total:".$listHotel->getTotalCost()."</li>
+                        
+                    </div>
+                ";`
+            };
+
+        ?>
+    </ul>
+</body>
+</html>
 
